6532a1f1cf219bfa475c88ff9e30afa56086b738
jiangliuzhu <740124694@qq.com>
2020-02-06 14:56:00 +0800

    上传项目到git

---
+++ src/main/java/com/xinke/epidemic_prevention/EpidemicPreventionApplication.java
@@ -0,0 +1,13 @@
+package com.xinke.epidemic_prevention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicPreventionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicPreventionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinke/epidemic_prevention/EpidemicPreventionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinke.epidemic_prevention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicPreventionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
