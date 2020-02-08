ec81894ab2a879fb352e20bf1b82af28f2063c07
15621583332 <w_yz2015@163.com>
2020-02-08 10:30:45 +0800

    汪元征：index页面及统计表格修改

--- src/main/java/com/xinke/epidemic_prevention/controller/formmanager/FormManager.java
+++ src/main/java/com/xinke/epidemic_prevention/controller/formmanager/FormManager.java
@@ -1,8 +1,16 @@
 package com.xinke.epidemic_prevention.controller.formmanager;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("form")
 public class FormManager {
-
+    @GetMapping("tz1")
+    public String tz1(){return "formmanage/form1";}
+    @GetMapping("tz2")
+    public String tz2(){return "formmanage/form2";}
+    @GetMapping("tz3")
+    public String tz3(){return "formmanage/form3";}
 }
